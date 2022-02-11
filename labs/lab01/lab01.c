162488f47465859a7cdc7a60abb2e51d46d79d35
declanquinn00 <quinndeclan00@gmail.com>
2022-02-11 15:27:34 +0000

    moved labs 01 and 02 to pico-apps-forked repository

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,11 +1,35 @@
-/**
- * @brief LAB #01 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
- */
+#include "pico/stdlib.h"
+
+ void LEDloop(const uint LED_PIN, const uint LED_DELAY);
+
 int main() {
 
-    // Returning zero indicates everything went okay.
+    // Specify the PIN number and sleep delay
+    const uint LED_PIN   =  25;
+    const uint LED_DELAY = 500;
+
+    // Setup the LED pin as an output.
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Do forever...
+    while (true) {
+        LEDloop(LED_PIN, LED_DELAY);
+    }
+
+    // Should never get here due to infinite while-loop.
     return 0;
+
 }
+
+//toggles LED on waits, then toggles LED off and waits
+void LEDloop( const uint LED_PIN, const uint LED_DELAY){
+        // Toggle the LED on and then sleep for delay period
+        gpio_put(LED_PIN, 1);
+        sleep_ms(LED_DELAY);
+
+        // Toggle the LED off and then sleep for delay period
+        gpio_put(LED_PIN, 0);
+        sleep_ms(LED_DELAY);
+}
+
--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -5,15 +5,54 @@
 #include "pico/float.h"     // Required for using single-precision variables.
 #include "pico/double.h"    // Required for using double-precision variables.
 
+float Wallis_Float();
+double Wallis_Double();
 
-/**
- * @brief LAB #02 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
- */
 int main() {
+
+#ifndef WOKWI
+    // Initialise the IO as we will be using the UART
+    // Only required for hardware and not needed for Wokwi
+    stdio_init_all();
+#endif
+    float pifloat = 3.14159265359;
+    double pidouble = 3.14159265359;
+    float faccuracy;
+    double daccuracy;
+    float fout = Wallis_Float();
+    printf("Float output: %f\n", fout);
+    faccuracy = pifloat - fout;
+    printf("Float output %f off PI\n", faccuracy);
+    double dout = Wallis_Double();
+    printf("Double output: %f\n", dout);
+    daccuracy = pidouble - dout;
+    printf("Double output %f off PI\n", daccuracy);
 
     // Returning zero indicates everything went okay.
     return 0;
 }
+
+//Calculates a float Pi approximation using Wallis algorithm
+float Wallis_Float(){
+    float out = 1.0f;
+    float tmp;
+    for(float i=1.0f;i<=100000.0f;i++){
+      tmp = 4.0f*(i*i);
+      out *= tmp/(tmp-1.0f);
+    }
+    out = out*2.0f;
+    return out;
+}
+
+//Calculates a double Pi approximation using Wallis algorithm
+double Wallis_Double(){
+    double out = 1.0;
+    double tmp;
+    for(double i=1.0;i<=100000.0;i++){
+      tmp = 4.0*(i*i);
+      out *= tmp/(tmp-1.0);
+    }
+    out = out*2.0;
+    return out;   
+}
+
